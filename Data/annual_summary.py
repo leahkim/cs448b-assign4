a41c90d0076cf77cb9fc6b4792740b09706a88d6
Ella Kim <ellakim2011@gmail.com>
2016-05-25 20:39:10 -0700

    restarting proj

--- Data/annual_summary.py
+++ Data/annual_summary.py
@@ -1,14 +1,13 @@
 import json
 
-yearset_array = [] 
-year_index = {}
+
 
 
 """
-Each year:
-- World's top terror groups by num incidents
-- Matrix of country-by-country relationship (separate json)
-- Per country 1) number of incidents 2) Top 5 terror groups and 3), {target: , count: }
+TODO: Fill in code to update 
 """
-with open("/Users/ellakim/Documents/classes/448b/cs448b-assign4/Data/kidnap.json", "rb") as infile:
-	
+
+with open("/Users/ellakim/Documents/classes/448b/cs448b-assign4/Data/kidnap_by_year.json", "rb") as infile:
+	# for each data entry (i.e. each yer)
+	# compute world top groups
+	# compute the list of country 
