55aa005ce8a10374259fae68e8567b5dc2ff41d5
jungsun@stanford.edu <ellakim2011@gmail.com>
2016-05-29 00:25:22 -0700

    basic survival rate plot working

--- survival_curve.js
+++ survival_curve.js
@@ -1,3 +1,10 @@
+var INIT_DEMAND = 1000000;
+var INIT_PAID = 40000;
+var selected_x = INIT_PAID / INIT_DEMAND;
+var selected_y = predict(selected_x);
+var x_text = selected_x < 0.1 ? '' : '% paid = ' + selected_x.toFixed(2);
+var y_text = 'rate = ' + selected_y.toFixed(2);
+
 var options = {
     title: 'Ransom Paid - Survival Rate',
     target: '#survival_graph',
@@ -17,30 +24,45 @@ var options = {
         derivative: {
             fn: '2x',
         }
-    }]
+    }],
+    annotations:
+        [{x: selected_x.toFixed(2), text: x_text},
+            {y: selected_y.toFixed(2), text: y_text}]
 };
 
 function predict(x) {
     return 1/(1 + Math.exp(-2.9854 * (x + 0.2892)));
 }
 
-var selected_x = 0.1;
-var selected_y = predict(selected_x);
 
-/** update call for element
-(function () {
-    x_text = 'x = ' + selected_x.toFixed(3);
-    y_text = 'y = ' + selected_y.toFixed(3);
-    options.annotations = [{x: selected_x.toFixed(3), text: x_text},
-                            {y: selected_y.toFixed(3), text: y_text}];
-    functionPlot(options)
-})
-**/
+function update_Plot(value) {
+    selected_x = value;
+    selected_y = predict(selected_x);
 
-x_text = selected_x < 0.1 ? '' : '% paid = ' + selected_x.toFixed(2);
-y_text = 'rate = ' + selected_y.toFixed(2);
-options.annotations = [{x: selected_x.toFixed(2), text: x_text},
-    {y: selected_y.toFixed(2), text: y_text}];
+    x_text = '% paid = ' + selected_x.toFixed(2);
+    y_text = 'rate = ' + selected_y.toFixed(2);
+
+    options.annotations = [{x: selected_x.toFixed(2), text: x_text},
+        {y: selected_y.toFixed(2), text: y_text}];
+
+    functionPlot(options);
+}
+
+$(document).ready(function() {
+    $("#slider").slider({
+        min: 0,
+        max: 1000000,
+        step: 1000,
+        values: [INIT_DEMAND, INIT_PAID],
+        slide: function(event, ui) {
+            for (var i = 0; i < ui.values.length; ++i) {
+                $("input.sliderValue[data-index=" + i + "]").val(ui.values[i]);
+            }
+            var portion = ui.values[1] / ui.values[0];
+            update_Plot(portion);
+        }
+    });
+});
 
 // Initial plot
 functionPlot(options);
